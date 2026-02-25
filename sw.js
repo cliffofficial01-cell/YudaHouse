19f58920065775bd8ea9258bc28b9a74650c8f23
cliffofficial01-cell <cliffofficial01@gmail.com>
2026-02-25 05:22:16 +0000

    first commit

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'yudahouse-v1';
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.jpg',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(resp => resp || fetch(e.request))
+  );
+});
